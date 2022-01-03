ab78c75ecaea7fa5c7622534f3260cba0ad24d3f
dave_id <djanur405@gmail.com>
2022-01-03 15:13:23 +0800

    add: section about dan destination

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,165 +1,172 @@
-/*============= SHOW MENU ===========*/
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
-
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
-if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
-  });
-}
-
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
-  });
-}
-
-/*======= REMOVE MENU MOBILE ========*/
-const navLink = document.querySelectorAll(".nav__link");
-
-function linkAction() {
-  const navMenu = document.getElementById("nav-menu");
-  // When we click on each nav__link, we remove the show-menu class
-  navMenu.classList.remove("show-menu");
-}
-navLink.forEach((n) => n.addEventListener("click", linkAction));
-
-/*======= CHANGE BACKGROUND HEADER ========*/
-function scrollHeader() {
-  const header = document.getElementById("header");
-
-  // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-  if (this.scrollY >= 100) header.classList.add("scroll-header");
-  else header.classList.remove("scroll-header");
-}
-window.addEventListener("scroll", scrollHeader);
-
-/*======= SWIPER DISCOVER (Initialize Swiper) ========*/
-var swiper = new Swiper(".discover__container", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  loop: true,
-  spaceBetween: 32,
-  coverflowEffect: {
-    rotate: 0,
-  },
+document.querySelectorAll(".about .about__video-container .about__control .control-btn").forEach((button) => {
+  button.onclick = () => {
+    let src = button.getAttribute("data-src");
+    document.querySelector(".about .about__video-container .about__video").src = src;
+  };
 });
 
-/*=============== VIDEO ==============*/
-const videoFile = document.getElementById("video-file"),
-  videoButton = document.getElementById("video-button"),
-  videoIcon = document.getElementById("video-icon");
+// /*============= SHOW MENU ===========*/
+// const navMenu = document.getElementById("nav-menu"),
+//   navToggle = document.getElementById("nav-toggle"),
+//   navClose = document.getElementById("nav-close");
 
-function playPause() {
-  if (videoFile.paused) {
-    // Play Video
-    videoFile.play();
+// /*===== MENU SHOW =====*/
+// /* Validate if constant exists */
+// if (navToggle) {
+//   navToggle.addEventListener("click", () => {
+//     navMenu.classList.add("show-menu");
+//   });
+// }
 
-    // change the icon
-    videoIcon.classList.add("ri-pause-line");
-    videoIcon.classList.remove("ri-play-line");
-  } else {
-    // Pause Video
-    videoFile.pause();
+// /*===== MENU HIDDEN =====*/
+// /* Validate if constant exists */
+// if (navClose) {
+//   navClose.addEventListener("click", () => {
+//     navMenu.classList.remove("show-menu");
+//   });
+// }
 
-    // change the icon
-    videoIcon.classList.remove("ri-pause-line");
-    videoIcon.classList.add("ri-play-line");
-  }
-}
-videoButton.addEventListener("click", playPause);
+// /*======= REMOVE MENU MOBILE ========*/
+// const navLink = document.querySelectorAll(".nav__link");
 
-function finalVideo() {
-  // Video ends, icon change
-  videoIcon.classList.remove("ri-pause-line");
-  videoIcon.classList.add("ri-play-line");
-}
-// ended, when the video ends
-videoFile.addEventListener("ended", finalVideo);
+// function linkAction() {
+//   const navMenu = document.getElementById("nav-menu");
+//   // When we click on each nav__link, we remove the show-menu class
+//   navMenu.classList.remove("show-menu");
+// }
+// navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*======= SHOW SCROLL UP ========*/
-function scrollUp() {
-  const scrollUp = document.getElementById("scroll-up");
-  // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
-  else scrollUp.classList.remove("show-scroll");
-}
-window.addEventListener("scroll", scrollUp);
+// /*======= CHANGE BACKGROUND HEADER ========*/
+// function scrollHeader() {
+//   const header = document.getElementById("header");
 
-/*======= SCROLL SECTION ACTIVE LINK ========*/
-const sections = document.querySelectorAll("section[id]");
+//   // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+//   if (this.scrollY >= 100) header.classList.add("scroll-header");
+//   else header.classList.remove("scroll-header");
+// }
+// window.addEventListener("scroll", scrollHeader);
 
-function scrollActive() {
-  const scrollY = window.pageYOffset;
+// /*======= SWIPER DISCOVER (Initialize Swiper) ========*/
+// var swiper = new Swiper(".discover__container", {
+//   effect: "coverflow",
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: "auto",
+//   loop: true,
+//   spaceBetween: 32,
+//   coverflowEffect: {
+//     rotate: 0,
+//   },
+// });
 
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight,
-      sectionTop = current.offsetTop - 50,
-      sectionId = current.getAttribute("id");
+// /*=============== VIDEO ==============*/
+// const videoFile = document.getElementById("video-file"),
+//   videoButton = document.getElementById("video-button"),
+//   videoIcon = document.getElementById("video-icon");
 
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active-link");
-    } else {
-      document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link");
-    }
-  });
-}
-window.addEventListener("scroll", scrollActive);
+// function playPause() {
+//   if (videoFile.paused) {
+//     // Play Video
+//     videoFile.play();
 
-/*========== DARK LIGHT THEME ==========*/
-const themeButton = document.getElementById("theme-button"); //? nama class sesuai id pada nav dark index.html
-const darkTheme = "dark-theme"; //? nama class sesuai yg ada di _theme.scss
-const iconTheme = "ri-sun-line"; //? icon remixicon.com
+//     // change the icon
+//     videoIcon.classList.add("ri-pause-line");
+//     videoIcon.classList.remove("ri-play-line");
+//   } else {
+//     // Pause Video
+//     videoFile.pause();
 
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
+//     // change the icon
+//     videoIcon.classList.remove("ri-pause-line");
+//     videoIcon.classList.add("ri-play-line");
+//   }
+// }
+// videoButton.addEventListener("click", playPause);
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? "dark" : "light");
-const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line");
+// function finalVideo() {
+//   // Video ends, icon change
+//   videoIcon.classList.remove("ri-pause-line");
+//   videoIcon.classList.add("ri-play-line");
+// }
+// // ended, when the video ends
+// videoFile.addEventListener("ended", finalVideo);
 
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
-  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](iconTheme);
-}
+// /*======= SHOW SCROLL UP ========*/
+// function scrollUp() {
+//   const scrollUp = document.getElementById("scroll-up");
+//   // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+//   if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
+//   else scrollUp.classList.remove("show-scroll");
+// }
+// window.addEventListener("scroll", scrollUp);
 
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener("click", () => {
-  // Add or remove the dark / icon theme
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
-  // We save the theme and the current icon that the user chose
-  localStorage.setItem("selected-theme", getCurrentTheme());
-  localStorage.setItem("selected-icon", getCurrentIcon());
-});
+// /*======= SCROLL SECTION ACTIVE LINK ========*/
+// const sections = document.querySelectorAll("section[id]");
 
-/*========== SCROLL REVEAL ANIMATION ==========*/
-const sr = ScrollReveal({
-  distance: "60px",
-  duration: 2800,
-  reset: true,
-});
+// function scrollActive() {
+//   const scrollY = window.pageYOffset;
 
-sr.reveal(`.home__data, .home__social-link, .discover__container, .experience__data, .experience__overlay, .place__card, .sponsor__content, .footer__data, .footer__rights`, {
-  origin: "top",
-  interval: 100,
-});
+//   sections.forEach((current) => {
+//     const sectionHeight = current.offsetHeight,
+//       sectionTop = current.offsetTop - 50,
+//       sectionId = current.getAttribute("id");
 
-sr.reveal(`.about__data, .video__description, .subscribe__description`, {
-  origin: "left",
-});
+//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//       document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active-link");
+//     } else {
+//       document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link");
+//     }
+//   });
+// }
+// window.addEventListener("scroll", scrollActive);
 
-sr.reveal(`.home__info, .about__img-overlay, .video__content, .subscribe__form`, {
-  origin: "right",
-  interval: 100,
-});
+// /*========== DARK LIGHT THEME ==========*/
+// const themeButton = document.getElementById("theme-button"); //? nama class sesuai id pada nav dark index.html
+// const darkTheme = "dark-theme"; //? nama class sesuai yg ada di _theme.scss
+// const iconTheme = "ri-sun-line"; //? icon remixicon.com
+
+// // Previously selected topic (if user selected)
+// const selectedTheme = localStorage.getItem("selected-theme");
+// const selectedIcon = localStorage.getItem("selected-icon");
+
+// // We obtain the current theme that the interface has by validating the dark-theme class
+// const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? "dark" : "light");
+// const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line");
+
+// // We validate if the user previously chose a topic
+// if (selectedTheme) {
+//   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+//   document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
+//   themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](iconTheme);
+// }
+
+// // Activate / deactivate the theme manually with the button
+// themeButton.addEventListener("click", () => {
+//   // Add or remove the dark / icon theme
+//   document.body.classList.toggle(darkTheme);
+//   themeButton.classList.toggle(iconTheme);
+//   // We save the theme and the current icon that the user chose
+//   localStorage.setItem("selected-theme", getCurrentTheme());
+//   localStorage.setItem("selected-icon", getCurrentIcon());
+// });
+
+// /*========== SCROLL REVEAL ANIMATION ==========*/
+// const sr = ScrollReveal({
+//   distance: "60px",
+//   duration: 2800,
+//   reset: true,
+// });
+
+// sr.reveal(`.home__data, .home__social-link, .discover__container, .experience__data, .experience__overlay, .place__card, .sponsor__content, .footer__data, .footer__rights`, {
+//   origin: "top",
+//   interval: 100,
+// });
+
+// sr.reveal(`.about__data, .video__description, .subscribe__description`, {
+//   origin: "left",
+// });
+
+// sr.reveal(`.home__info, .about__img-overlay, .video__content, .subscribe__form`, {
+//   origin: "right",
+//   interval: 100,
+// });
